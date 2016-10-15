9ae815b1d62065d3c33f97b6d354d0c0de8f5065
Steve <steve@wotid.com>
2016-10-15 16:26:31 +0000

    Commit 1

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,16 @@
-var aloha = 'Aloha Variables';
-console.log(aloha);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.get('/', function(req, res){
+    res.send('Aloha World!');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Aloha Routing!');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+});
